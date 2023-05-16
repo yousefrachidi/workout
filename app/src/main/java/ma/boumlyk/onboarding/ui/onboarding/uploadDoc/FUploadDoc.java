ca73837a04b583b13861919d2c186dbbf9906254
youssef rachidi <youssef.rachidi@indatacore.com>
2023-05-16 02:11:19 +0100

    edite from page upload doc

---
+++ app/src/main/java/ma/boumlyk/onboarding/ui/onboarding/uploadDoc/FUploadDoc.java
@@ -0,0 +1,78 @@
+package ma.boumlyk.onboarding.ui.onboarding.uploadDoc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
+
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import ma.boumlyk.onboarding.R;
+import ma.boumlyk.onboarding.databinding.FRegisterPhoneBinding;
+import ma.boumlyk.onboarding.databinding.FUploadDocBinding;
+import ma.boumlyk.onboarding.ui.BaseActivity;
+import ma.boumlyk.onboarding.ui.BaseFragment;
+
+public class FUploadDoc extends BaseFragment {
+
+
+    FUploadDocBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.f_upload_doc, container, false);
+        viewModel = new ViewModelProvider(this).get(FUploadDocViewModel.class);
+        binding.setViewModel((FUploadDocViewModel) viewModel);
+        binding.setLifecycleOwner(this);
+        initiateView();
+        initiateObservers();
+        return binding.getRoot();
+    }
+
+    private void initiateObservers() {
+        ((FUploadDocViewModel) viewModel).initiateViewModel((BaseActivity) requireActivity() );
+        viewModel.actions.observe(requireActivity(), actions -> {
+            for (String action : actions) {
+                switch (action) {
+                   
+                    case BaseActivity.ACTION_FINISH_LOADING:
+                        onFinishLoading();
+                        break;
+                    case BaseActivity.ACTION_START_LOADING:
+                        onStartLoading();
+                        break;
+                }
+            }
+        });
+//        slideDown(binding.relativeLayout2);
+        viewModel.animateViewFromBottomToTop(binding.relativeLayout2);
+
+    }
+
+    private void initiateView() {
+
+    }
+
+    public void slideDown(View view) {
+        view.setVisibility(View.INVISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(0, 0, view.getHeight(), 0);
+        animate.setDuration(1500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+      //  view.setVisibility(View.VISIBLE);
+    }
+
+
+
+
+    private void onStartLoading() {
+       // UITools.setOnLoadingState(true, requireActivity(), binding.footerLayout.btnCnx, R.id.btn_progress, R.id.btn_next);
+    }
+
+    private void onFinishLoading() {
+        //UITools.setOnLoadingState(false, requireActivity(), binding.footerLayout.btnCnx, R.id.btn_progress, R.id.btn_next);
+    }
+}
---
+++ app/src/main/java/ma/boumlyk/onboarding/ui/onboarding/uploadDoc/FUploadDocViewModel.java
@@ -0,0 +1,87 @@
+package ma.boumlyk.onboarding.ui.onboarding.uploadDoc;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.Collections;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import ma.boumlyk.onboarding.ui.BaseActivity;
+import ma.boumlyk.onboarding.ui.BaseViewModel;
+import ma.boumlyk.onboarding.ui.onboarding.registerInfo.FRegisterInfo;
+import ma.boumlyk.onboarding.ui.onboarding.support.FSupport;
+
+@HiltViewModel
+public class FUploadDocViewModel extends BaseViewModel {
+
+    String[] addresses = {"123 Main St, Anytown USA", "456 Oak St, Anycity USA", "789 Pine St, Anyvillage USA"};
+
+    boolean isPhoneNumberValid = false;
+    boolean isPhoneNumberEmpty = true;
+
+    EditText editPhoneNumber;
+
+
+    @Inject
+    public FUploadDocViewModel() {
+    }
+
+    public void initiateViewModel(BaseActivity requireActivity, EditText editPhoneNumber) {
+        super.initiateViewModel(requireActivity);
+        this.editPhoneNumber=editPhoneNumber;
+    }
+
+    public void onCreateAccount() {
+
+
+    }
+
+    public void onNumberKeyboardClick(View v) {
+
+
+
+
+    }
+
+    public void onPhoneTextChanged(CharSequence s, int start, int before, int count) {
+        isPhoneNumberEmpty = s.toString().isEmpty();
+        actions.postValue(Collections.singletonList(BaseActivity.ACTION_ON_PHONE_TYPING));
+    }
+
+
+    public EditText.OnFocusChangeListener getOnPhoneFocusChanged() {
+        return (v, hasFocus) -> {
+            if ((!hasFocus) && (!isPhoneNumberEmpty)) {
+                if (!isPhoneNumberValid)
+                    actions.postValue(Collections.singletonList(BaseActivity.ACTION_PHONE_NOT_VALID));
+
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        };
+    }
+
+    // Method to hide the soft keyboard
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public void onNeedSupport() {
+        fragment.postValue(new FSupport());
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        fragment.postValue(new FRegisterInfo());
+    }
+
+
+}
